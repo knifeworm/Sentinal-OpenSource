6560d3db5baa1bda5ca2c857271037faf9d21797
knifeworm <77696553+knifeworm@users.noreply.github.com>
2022-01-05 18:29:13 +0000

    Updated moderation.py

--- cogs/moderation.py
+++ cogs/moderation.py
@@ -20,7 +20,7 @@ class Moderation(commands.cog):
     @commands.has_permissions(manage_messages = True)
     async def clear(self, ctx, amount = 5):
         embed = nextcord.Embed(title="Moderation | Clear",description=f"{amount} messages were cleared!",color=0x61FBFB)
-        embed.set_footer(text=f"Information requested by {ctx.author.display_name}",icon_url=f"{ctx.author.avatar_url} {round(client.latency * 1000)}ms")
+        embed.set_footer(text=f"Information requested by {ctx.author.display_name}",icon_url=f"{ctx.author.avatar_url} {round(self.client.latency * 1000)}ms")
         await ctx.channel.purge(limit=amount)
 
 
